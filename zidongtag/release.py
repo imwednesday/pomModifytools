479ef26ca5776c60493c7541290ba568f997ccc5
hotaru <longliveccp@qq.com>
2019-11-04 15:37:25 +0800

    初步实现

--- zidongtag/release.py
+++ zidongtag/release.py
@@ -11,41 +11,52 @@ import os
 import subprocess
 import time
 
-# 1.事先准备的tag文件目录,它是自己准备的,编码为uft-8
-path1 = "C:\\Users\\dell\\Desktop\\CM491\\491.txt"
 
-# f1path = input('请输入地址:')
+def zidong():
+    # 获取记录了模块路径的文本 return path
+    getPath()
+    # 对pomlist循环处理,即执行mvn及svn相关命令
+    doMS()
+
+
+# 获取记录了模块路径的文本 path
+def getPath():
+    global txtPath
+    # txtPath=input('请输入文本绝对路径')
+    txtPath = "C:\\Users\\dell\\Desktop\\CM491\\491.txt"
+    global linesList
+    linesList = fileRead(txtPath)
+    # return txtPath
+    # 3.读取pom,删除其中的SNAPSHOT标记
+    global absPomList
+    absPomList = []  # 一个包含了所有需要执行MS命令的pom的绝对路径list
+    for p in linesList:
+        if p.strip() != "":
+            p1 = os.path.dirname(p)  # 将字符串转换为路径
+            dirName = os.listdir(p1)  # 下一层必然有pom,代码就是为了修改它
+            for k in dirName:
+                if (k == 'pom.xml'):  # 生成pom的绝对路径
+                    absPomList.append(os.path.join(p1, k))
 
 
 # 读文件方法
 def fileRead(fileName):
-    fread = open(fileName, 'r', encoding="utf-8")  # 读
+    fread = open(fileName, 'r', encoding="utf-8")  # 一个读
     lines = fread.readlines()  # 按行读取内容
     fread.close()
     return lines
 
 
-# 2.读取txt文件,遍历lines
-linesList = fileRead(path1)
-absPomList = []
-# 3.读取pom,删除其中的SNAPSHOT标记
-for p in linesList:
-    if p.strip() != "":
-        # print(p)
-        p1 = os.path.dirname(p)  # 将字符串转换为路径
-        dirName = os.listdir(p1)  # 下一层必然有pom,代码就是为了修改它
-        for k in dirName:
-            if (k == 'pom.xml'):  # 生成pom的绝对路径
-                absPomList.append(os.path.join(p1, k))
-
-# 对应3rd和parent的tag,单独处理,此外还有一些需要单独处理的模块
-for pom in absPomList:
-    newPOM = []
-    lines = fileRead(pom)
-    # newPOM=lines
+# 写文件方法
+def fileWrite(fileName, lines):
+    fwrite = open(fileName, 'w', encoding="utf-8")  # 一个写
     for line in lines:
-        newPOM.append(line)
-    # 对lines处理两遍,第一遍单独处理3rd及parent
+        fwrite.write(line)
+    fwrite.close()
+
+
+# 对应3rd和parent的tag,单独处理,这个循环只进行一次,此外还有一些需要单独处理的模块
+def is3RD(lines, newPOM):
     for line in lines:
         # 先判断SNAPSHOT
         if 'SNAPSHOT' in line:
@@ -54,6 +65,11 @@ for pom in absPomList:
             del newPOM[i]
             newPOM.insert(i, line)
             break
+    return newPOM
+
+
+# 如果此次不包含3rd,则除本模块外,去掉pom中的SNAPSHOT
+def isNot3RD(lines, newPOM):
     # 跳过前15行,这其中包含本模块的gav
     for line in newPOM[15:]:
         if 'SNAPSHOT' in line:
@@ -61,53 +77,70 @@ for pom in absPomList:
             line = line.replace('-SNAPSHOT', '')
             del lines[i]
             lines.insert(i, line)
-    # 对两个list进行组合,得到最终结果
-    lines = newPOM[:15] + lines[15:]
-    # 对pom的修改
-    fwrite = open(pom, 'w', encoding="utf-8")  # 一个写
-    for line in lines:
-        fwrite.write(line)
-    fwrite.close()
-    # 3.执行一次命令,删除文本内及lines内一条目录(关键)
-    mvn3 = "mvn clean"
-    mvnT1 = "mvn package"
-    # str2 = "E: && cd " + f1path + " && dir && " + mvnT1
-    # p = subprocess.Popen(str2, shell=True)
-    # p.wait()
-    # 对sonar进行修改
-    # moduleName=f1path.split("\\")[-1]
-    for index, line in enumerate(lines):
-        if 'TEST' in line:
-            k = ''.join(lines[index].split())
-            version = k[9:-15]
-            # print(version)
-    # 生成sonar文件路径
-    sonaFile = os.path.join(linesList[0][:-1], "sonar-project.properties")
-    # print(sonaFile)
-    # print(os.path.isfile(sonaFile))
-    fread = open(sonaFile, 'r', encoding="utf-8")  # 读
-    sonarLinesBE = fread.readlines()
-    fread.close()
-    sonarLinesAF = []
-    for line in sonarLinesBE:
-        sonarLinesAF.append(line)
-    sonarLinesAF[5] = sonarLinesAF[5][:-6] + version + '\n'
-    fwrite = open(sonaFile, "w", encoding="utf-8")
-    for line in sonarLinesAF:
-        fwrite.write(line)
-    fwrite.close()
-    fread = open(sonaFile, 'r', encoding="utf-8")  # 读
-    sonarLinesAF = fread.readlines()
-    print(sonarLinesAF)
-    time.sleep(100)
-    # 对txt的修改
-    fwrite = open(path1, "w", encoding="utf-8")
-    del linesList[0]
-    for line in linesList:
-        fwrite.write(line)
-    fwrite.close()
-    str1 = input("如果继续请输入\"1\"")
-    if str1 != 1:
-        quit()
+    return lines
 
-# 3.执行一次命令,删除文本内及lines内一条目录(关键)
+
+# sonar文件的修改
+def sonarEdit(lines):
+    version = ""
+    for index, line in enumerate(lines):
+        if 'SNAPSHOT' in line:
+            k = ''.join(lines[index].split())
+            version = k[9:-19]
+    # 为保证文件对应关系不出错,需要对txt文本进行修改,每完成一次MS就删除一行,txt的第一行必然是正在修改的pom
+    sonaFile = os.path.join(linesList[0][:-1], "sonar-project.properties")
+    sonarLines = fileRead(sonaFile)
+    sonarLines[5] = sonarLines[5][:-6] + version + '\n'
+    fileWrite(sonaFile, sonarLines)
+
+
+def mavenAndsvn(pomDir):
+    p = os.path.dirname(pomDir)  # 将字符串转换为路径
+
+    mvnLog = " >> C:\\Users\\dell\\Desktop\\CM491\\mvn.txt"
+    svnLog = " >> C:\\Users\\dell\\Desktop\\CM491\\svn.txt"
+    cdE = "E: && cd " + p
+    addition = " && "
+    mvn1 = "mvn release:prepare -Darguments=\"-DskipTests\"" + mvnLog
+    mvn2 = "release:perform -Darguments=\"-Dmaven.javadoc.skip=true\"" + mvnLog
+    mvn3 = "mvn clean" + mvnLog
+    mvnT1 = "mvn package" + mvnLog
+    svnUptade = "svn update" + svnLog
+    svnCommit = "svn ci -m " + "\"【问题单号】：无 【简要描述】：测试提交记录\" pom.xml" + svnLog
+    str1 = cdE + addition + svnUptade + addition + "dir" + addition + mvn3
+    # shell=True的作用是接收字符串作为指令
+    p = subprocess.Popen(str1, shell=True)
+    p.wait()
+
+
+def doMS():
+    # 如果本次模块包含3rd,需要先进行一次处理
+    str3rd = input('本次是否包含3rd及parent模块,如果是请输入\'1\',如果不包含,请输入\'0\':')
+    for pom in absPomList:
+        lines = fileRead(pom)
+        newPOM = []
+        for line in lines:
+            newPOM.append(line)
+        if str3rd == '1':
+            newPOM = is3RD(lines, newPOM)
+        lines = isNot3RD(lines, newPOM)
+        # 对两个list进行组合,得到最终结果
+        lines = newPOM[:15] + lines[15:]
+        # 对pom的修改
+        fileWrite(pom, lines)
+        # 对sonar文件的修改
+        sonarEdit(lines)
+        # 执行MS命令
+
+        mavenAndsvn(linesList[0])
+        # 对txt的修改
+        del linesList[0]
+        # del absPomList[0]
+        fileWrite(txtPath, linesList)
+
+        str1 = input("如果继续请输入\'1\',如果需要终止,请输入\'0\':")
+        if str1 != '1':
+            quit()
+
+
+zidong()
